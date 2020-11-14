ffb4d4cb754a2dcbf8aa8cb1ccabc2c331bf570a
totegsito <pacoquesmun@gmail.com>
2020-11-14 19:13:02 +0100

    pagination :hammer:

--- electronApp/views/editDeck/index.js
+++ electronApp/views/editDeck/index.js
@@ -6,8 +6,50 @@ const cardsToShow = 8;
 let indexOfCardsShown = 0;
 let pageNumber = 0;
 
+const nextPageButton = document.getElementById('rightArrowContainer')
+const previousPageButton = document.getElementById('leftArrowContainer')
+
+const isLastPage = () => (pageNumber === Math.floor(cards[0].length / cardsToShow) + 1)
+
+const cardsResultsContainer = document.querySelector(".editDeck_cards_results");
+
+const emptyResults = () => {
+  cardsResultsContainer.innerHTML = '';
+}
+
+const handleNextPageButtonVisiblity = () => {
+  if (isLastPage()) {
+    if (!nextPageButton.classList.contains('hide-arrow')) {
+      nextPageButton.classList.add('hide-arrow');
+    }
+  } else {
+    if (nextPageButton.classList.contains('hide-arrow')) {
+      nextPageButton.classList.remove('hide-arrow');
+    }
+  }
+};
+
+const handlePreviousPageButtonVisiblity = () => {
+  if (pageNumber === 1) {
+    if (!previousPageButton.classList.contains('hide-arrow')) {
+      previousPageButton.classList.add('hide-arrow');
+    }
+  } else {
+    if (previousPageButton.classList.contains('hide-arrow')) {
+      previousPageButton.classList.remove('hide-arrow');
+    }
+  }
+};
+
+const updateView = () =>{
+  emptyResults();
+  handleNextPageButtonVisiblity();
+  handlePreviousPageButtonVisiblity();
+}
+
+
 const getCards = () => {
-  const cardsFound = apiCaller
+  return apiCaller
     .getCardsByColor("white")
     .then((response) => {
       console.log("Response", response);
@@ -18,8 +60,8 @@ const getCards = () => {
     .catch((error) => {
       console.log(error);
     });
-  return cardsFound;
 };
+
 const printCard = (card) => {
   const cardImg = document.createElement("img");
   cardImg.setAttribute("src", card.image_uris.border_crop);
@@ -27,86 +69,36 @@ const printCard = (card) => {
   return cardImg;
 };
 
-const printCardsFound = () => {
-  const cardsResultsContainer = document.querySelector(
-    ".editDeck_cards_results"
-  );
-  const leftArrow = document.querySelector(".fa-arrow-left");
-  leftArrow.classList.add("hide-arrow");
-  let i;
+const getCardAmount = () => isLastPage() ? (cards[0].length - 1) : (cardsToShow * pageNumber);
 
-  for (i = indexOfCardsShown; i < cardsToShow; i++) {
-    let cardsFound = printCard(cards[0][i]);
+const goToNextPage = () => {
+  pageNumber += 1;
+  updateView();
+  const maximum = getCardAmount();
+  for(let i = indexOfCardsShown; i < maximum; i += 1) {
+    const cardsFound = printCard(cards[0][i]);
     cardsResultsContainer.appendChild(cardsFound);
   }
-  pageNumber = 1;
-  indexOfCardsShown = i ;
-  console.log({indexOfCardsShown});
+
+  indexOfCardsShown = maximum;
+  return indexOfCardsShown
+};
+
+const goToPreviousPage = () => {
+  pageNumber -= 1;
+  updateView();
+  indexOfCardsShown = cardsToShow * pageNumber;
+  for(let i = indexOfCardsShown - cardsToShow; i < indexOfCardsShown; i += 1) {
+    const cardsFound = printCard(cards[0][i]);
+    cardsResultsContainer.appendChild(cardsFound);
+  }
   return indexOfCardsShown;
-  //   cards.cards.forEach((card) => {
-  //     cardsFound = printCard(card);
-  //     cardsResultsContainer.appendChild(cardsFound);
-  //   });
-};
-
-const nextCardPage = () => {
-  const cardsResultsContainer = document.querySelector(
-    ".editDeck_cards_results"
-  );
-
-  cardsResultsContainer.innerHTML = "";
-  indexOfCardsShown += 1;
-  let cardsToShow = indexOfCardsShown + 7;
-  document
-  .getElementById("leftArrowContainer")
-  .classList.remove("hide-arrow");
-
-  for (
-    indexOfCardsShown;
-    indexOfCardsShown <= cardsToShow;
-    indexOfCardsShown++
-  ) {
-    cardsFound = printCard(cards[0][indexOfCardsShown]);
-    cardsResultsContainer.appendChild(cardsFound);
-  }
-  pageNumber++;
-  indexOfCardsShown -= 1;
-  console.log({indexOfCardsShown});
-};
-
-const previousCardPage = (e) => {
-  const cardsResultsContainer = document.querySelector(
-    ".editDeck_cards_results"
-  );
-  const leftArrow = document.querySelector(".fa-arrow-left");
-  indexOfCardsShown -= 14;
-
-  cardsResultsContainer.innerHTML = "";
-  // let cardsToShow = indexOfCardsShown + 8;
-  for (
-    indexOfCardsShown;
-    indexOfCardsShown <= indexOfCardsShown + cardsToShow;
-    indexOfCardsShown++
-  ) {
-    if (indexOfCardsShown >= 8) {
-      document
-        .getElementById("leftArrowContainer")
-        .classList.remove("hide-arrow");
-    }
-    if(indexOfCardsShown === 8){
-      document
-      .getElementById("leftArrowContainer")
-      .classList.add("hide-arrow");
-    }
-    cardsFound = printCard(cards[0][indexOfCardsShown]);
-    cardsResultsContainer.appendChild(cardsFound);
-  }
-  indexOfCardsShown -= 1;
-  pageNumber--;
 };
 
 (async () => {
   await getCards();
-  printCardsFound();
+  nextPageButton.addEventListener('click', goToNextPage);
+  previousPageButton.addEventListener('click', goToPreviousPage);
+  goToNextPage();
 })();
 // getCards();
